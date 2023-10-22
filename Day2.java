ba9d2950bb954fb40c405131cb286bd2b0cbdabc
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-22 19:28:36 +0700

    tipe data

---
+++ Day2.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class Day2 {
+    public static void main(String[] args) {
+        String nama = "Rizky adiwijaya";
+        int umur = 20;
+        String prodi = "INFORMATIKA";
+        String NIM = "D0222045";
+        
+        System.out.println("Nama :"+nama);
+        System.out.println("Umur :"+umur);
+        System.out.println("Program Study :"+prodi);
+        System.out.println("NIM :"+NIM);
+        
+    }
+}
