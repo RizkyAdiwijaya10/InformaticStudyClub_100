bbb5234280993c269b40469d01eb3fdf1ec36cce
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-22 19:31:12 +0700

    concat

---
+++ Day4.java
@@ -0,0 +1,10 @@
+package ISC;
+
+public class Day4 {
+    public static void main(String[] args) {
+        String str1 = "Rizky ";
+        String str2 = "Adiwijaya";
+        String str3 = str1.concat(str2);
+        System.out.println(""+str3);
+    }
+}
