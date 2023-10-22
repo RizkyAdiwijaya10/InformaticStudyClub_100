ee99be66a23756563826999557364e1ce577e37b
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-22 20:02:48 +0700

    multiple method in main class

---
+++ day10.java
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+package informatic_study_club;
+import java.util.Scanner;
+public class day10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Nilai n : ");
+        double n = ayamBawang(sc.nextDouble());
+        System.out.println(n);
+        System.out.print("Nilai m : ");
+        int m = baksoMercon(sc.nextInt());
+        System.out.println(m);
+        
+    }
+
+    public static double ayamBawang(double n) {
+        System.out.println(2 * n);
+        return n;
+
+    }
+
+    public static int baksoMercon (int m) {
+        System.out.println(5 + m);
+        return m;
+    }
+    
+}
+=======
+package informatic_study_club;
+import java.util.Scanner;
+public class day10 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Nilai n : ");
+        double n = ayamBawang(sc.nextDouble());
+        System.out.println(n);
+        System.out.print("Nilai m : ");
+        int m = baksoMercon(sc.nextInt());
+        System.out.println(m);
+        
+    }
+
+    public static double ayamBawang(double n) {
+        System.out.println(2 * n);
+        return n;
+
+    }
+
+    public static int baksoMercon (int m) {
+        System.out.println(5 + m);
+        return m;
+    }
+    
+}
+>>>>>>> c9239a65444650697274aa8f0d9c2d222a3a7c48
