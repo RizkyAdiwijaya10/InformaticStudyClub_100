1bc86cd7a571ac23556d910059b0cad42b4b26ca
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-22 20:09:26 +0700

    ArrayList<integer>

---
+++ day13.java
@@ -0,0 +1,25 @@
+package informatic_study_club;
+
+import java.util.ArrayList;
+
+public class day13 {
+
+    public static void main(String[] args) {
+        ArrayList <Integer> data = new ArrayList<>();
+        data.add(12);
+        data.add(104);
+        data.add(4390);
+        data.add(897);
+        data.add(45);
+
+        System.out.print("[");
+        for (int i = 0; i < data.size(); i++) {
+            if (i > 0) {
+                System.out.print(", ");
+            }
+            System.out.print(data.get(i));
+        }
+        System.out.print("]");
+    }
+
+}
