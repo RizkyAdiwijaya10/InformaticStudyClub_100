f203829546841eee92544a8442376c00d253a3b0
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-22 19:30:08 +0700

    i < 1001

---
+++ Day3.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class Day3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1001; i++) {
+            System.out.println("RIZKY ADIWIJAYA");
+            
+        }
+    }
+    
+}
