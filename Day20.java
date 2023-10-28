f29a8cdf3ff6f10208c4b6ca769d05cd0d241187
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-28 20:34:38 +0700

    Integer to String

---
+++ Day20.java
@@ -0,0 +1,10 @@
+package informatic_study_club;
+
+
+public class Day20 {
+    public static void main(String[] args) {
+        int data = 123456789;
+        String dataString = Integer.toString(data);
+        System.out.println(dataString);
+    }
+}
