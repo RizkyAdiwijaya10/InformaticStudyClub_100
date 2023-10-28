39b6d6b46602939bea34877ee503c5342a49f0d7
RizkyaAdiwijaya10 <rkyywx@gmail.com>
2023-10-28 20:19:03 +0700

    void dan non-void

---
+++ Day19.java
@@ -0,0 +1,17 @@
+package informatic_study_club;
+
+public class Day19 {
+        static String nama= "Rizky adiwijaya";
+    
+    static void umur(){
+        System.out.println(20);
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        System.out.println(Day19.nama);
+        
+        Day19.umur();
+    }
+    
+}
