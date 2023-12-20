f9dc04545d14b58732b1df1a7da8815d122b5494
RizkyAdiwijaya10 <147222885+RizkyAdiwijaya10@users.noreply.github.com>
2023-12-20 21:54:49 +0800

    Add files via upload

---
+++ Day73.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Day73{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Value : ");
+        int value = sc.nextInt();
+
+        if(value >= 90){
+            System.out.println("Nilai : A ");
+        }else if(value >= 80  ){
+            System.out.println("Nilai : B ");
+        }else if(value >= 70){
+            System.out.println("Nilai : C ");
+        }else if(value >= 60){
+            System.out.println("Nilai : D ");
+        }else if(value >= 50){
+            System.out.println("Nilai : E ");
+        }else{
+            System.out.println("Error");
+        }
+        
+
+    }
+
+}
